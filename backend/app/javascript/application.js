d94afc01c5c17241f295a619971aca452ffa0148
Iván Madera <maderamartinezivan@gmail.com>
2025-06-06 12:00:08 -0500

    cambios

--- backend/app/javascript/application.js
+++ backend/app/javascript/application.js
@@ -3,6 +3,27 @@ import "@hotwired/turbo-rails";
 import "controllers";
 
 document.addEventListener("turbo:load", () => {
+  feather.replace();
+
+  //* === MODO OSCURO: Iconos de cambio de tema ===
+  const sidebar = document.getElementById("sidebar");
+  const toggleBtn = document.getElementById("sidebarToggle");
+  const backdrop = document.getElementById("backdrop");
+
+  if (toggleBtn) {
+    toggleBtn.addEventListener("click", () => {
+      sidebar.classList.toggle("active");
+      backdrop.style.display = sidebar.classList.contains("active")
+        ? "block"
+        : "none";
+    });
+  }
+
+  backdrop.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+    backdrop.style.display = "none";
+  });
+
   //* === MODO OSCURO ===
   const toggleButton = document.getElementById("theme-toggle");
   const themeToggleXl = document.getElementById("theme-toggle-xl");
