2470c822b48222fa6e883e24e4dc91bd0c797e19
Bastianwx <sebastiancera2005@gmail.com>
2025-06-25 12:45:00 -0500

    new mocki

--- frontend/lib/feature/categories/repositories/category_repository.dart
+++ frontend/lib/feature/categories/repositories/category_repository.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import '../models/category_model.dart';
 
 class CategoryRepository {
-  final String _baseUrl = 'https://run.mocky.io/v3/e0389d11-5e31-42c7-bf5d-d1ba73e0dee0';
+  final String _baseUrl = 'https://mocki.io/v1/fd8115f4-f0c0-407d-ba6d-865da1364433';
 
   Future<List<CategoryModel>> getCategories() async {
     final response = await http.get(Uri.parse(_baseUrl));
