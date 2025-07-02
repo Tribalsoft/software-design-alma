287516397d0d026c4bae7ebab12caecec2ffeb13
Bastianwx <sebastiancera2005@gmail.com>
2025-07-01 20:41:21 -0500

    arreglos routes

--- frontend/lib/routes/routes.dart
+++ frontend/lib/routes/routes.dart
@@ -1,14 +1,14 @@
 import 'package:design_alma/feature/profile/pages/profile_page.dart';
+import 'package:design_alma/feature/login/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
-import '../app/main_scaffold.dart';
-
 class AppRoute {
-  static const String home = '/';
+  static const String home = '/login';
   static const String profile = '/profile';
+  static const String login = '/login';
 
   final Map<String, WidgetBuilder> routes = {
-    AppRoute.home: (context) => MainScaffold(),
-    AppRoute.profile: (context) => const PerfilPage(),
+    login: (context) => const LoginScreen(),
+    profile: (context) => const PerfilPage(),
   };
 }
