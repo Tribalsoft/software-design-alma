88e322092067315f4e5195f42ec3ff3fbfc6df45
Bastianwx <sebastiancera2005@gmail.com>
2025-07-01 20:15:16 -0500

    url

--- frontend/lib/feature/login/services/auth_service.dart
+++ frontend/lib/feature/login/services/auth_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 class AuthService {
   Future<Map<String, dynamic>?> login(String email, String password) async {
     final response = await http.get(
-      Uri.parse("")
+      Uri.parse("https://mocki.io/v1/51dfd773-1fd6-4452-9e6b-dc61869f2fb9")
     );
 
     if (response.statusCode == 200) {
